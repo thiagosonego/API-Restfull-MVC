a6a2dee79c59c71da7ebffe6753c78b5d445207e
thiagosonego <thiagorodrigosonego@hotmail.com>
2025-06-18 19:34:36 -0300

    Criação do projeto MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio_Final_Arquitetura_de_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
